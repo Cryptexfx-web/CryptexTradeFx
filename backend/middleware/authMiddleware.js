bd4c2634061e5d155e0f2f0a14c3bcfb12608622
Cryptexfx-web <4kccfdvzzm@privaterelay.appleid.com>
2025-12-15 20:55:44 +0100

    Create authMiddleware.js

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (role) => (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json({ message: "No token" });
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  if (role && decoded.role !== role)
+    return res.status(403).json({ message: "Access denied" });
+
+  req.user = decoded;
+  next();
+};
